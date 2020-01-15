ecc12bdec169b01996435e05599953dfddac135f
skykchmin <43941493+skykchmin@users.noreply.github.com>
2020-01-15 23:21:45 +0900

    02. MTV - 07. MTV의 T,V 만들기(3) - 회원가입
    회원가입하기, 비밀번호가 비밀번호확인이랑 다를경우 , admin의 비밀번호 암호화, 회원가입시 빈 문자열이 입력되었을 때 예외 처리
    
    [200115]

--- 3강. Framework Django 기초/django/fcdjango_venv/fc_community/fcuser/views.py
+++ 3강. Framework Django 기초/django/fcdjango_venv/fc_community/fcuser/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.hashers import make_password
 from .models import Fcuser
 # Create your views here.
 
@@ -9,21 +10,24 @@ def register(request): #등록화면 장고에 등록하기
     if request.method == 'GET': 
         return render(request, 'register.html') #반환하고 싶은 html을 반환
     elif request.method == 'POST':
-        username = request.POST.get('username') #html로부터 name 값으로 값들이 전달됨
-        password = request.POST.get('password') #html로부터 name 값으로 값들이 전달됨
-        re_password = request.POST.get('re_password') #html로부터 name 값으로 값들이 전달됨
+        username = request.POST.get('username', None) #html로부터 name 값으로 값들이 전달됨
+        password = request.POST.get('password', None) #html로부터 name 값으로 값들이 전달됨
+        re_password = request.POST.get('re-password', None) #html로부터 name 값으로 값들이 전달됨
 
         res_data = {}
+
+        if not (username and password and re_password):
+            res_data['error'] = '모든 값을 입력해야합니다'
         if password != re_password:
-            res_data['error'] = '비밀번호가 다릅니다!'
+            res_data['error'] = '비밀번호가 다릅니다'
         else:
             fcuser = Fcuser( #객체를 만들고
                 username = username,
-                password = password
-            )
+                password = make_password(password) # make_password를 통해 해쉬를 통해 암호화가 된다
+            ) 
 
             fcuser.save() #객체를 세이브하고 데이터베이스에 생성이 되는지
 
-        return render(request, 'register.html') #반환하고 싶은 html을 반환 
+        return render(request, 'register.html', res_data) #반환하고 싶은 html을 반환, res_data는 에러메시지를 전달하기 위해서 
    
 
