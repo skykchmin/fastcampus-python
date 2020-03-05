d074a358cc3f29f7d80773ead3a79943986a49d2
skykchmin <43941493+skykchmin@users.noreply.github.com>
2020-03-05 16:31:04 +0900

    03-05. 게시판 만들기 3 - 예외처리
    03-05. 게시판 만들기 3 - 예외처리
    
    paginator를 이용한 페이지 만들기, 아이디 없을 때 user 예외처리, 사용자 없을떄 로그인창으로 이동
    
    [200305]

--- 3강. Framework Django 기초/django/fcdjango_venv/fc_community/board/views.py
+++ 3강. Framework Django 기초/django/fcdjango_venv/fc_community/board/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render, redirect
+from django.core.paginator import Paginator # 페이지 넘기는것
+from django.http import Http404
 from fcuser.models import Fcuser
 from .models import Board
 from .forms import BoardForm
 
 def board_detail(request, pk): # 첫번째 글인지, 두번째 글인지 입력을 받아야함 그래서 pk 필요
-    board = Board.objects.get(pk=pk) # pk에 해당하는 글을 가져올 수 있다
+    try:
+        board = Board.objects.get(pk=pk) # pk에 해당하는 글을 가져올 수 있다
+    except Board.DoesNotExist:
+        raise Http404('게시글을 찾을 수 없습니다')
+
     return render(request, 'board_detail.html', {'board': board})
 
 # Create your views here.
 def board_write(request): #제목과 내용이 요청되었을 때 작성자 정보까지 저장
+    if not request.session.get('user'): #유저가 없을때 글이 써지지 않고 로그인으로 가게
+        return redirect('/fcuser/login/')
+
     if request.method == 'POST': #글 쓸 때 로그인이 되어있어야함 
         form = BoardForm(request.POST)
         if form.is_valid():
@@ -28,7 +37,9 @@ def board_write(request): #제목과 내용이 요청되었을 때 작성자 정
     return render(request, 'board_write.html', {'form': form})
 
 def board_list(request):
-    boards = Board.objects.all().order_by('-id')
-    # -는 역순을 의미
-    # 모든 게시물을 가지고 올껀데 역순이여서 최신순으로 가져올 것이다. 
+    all_boards = Board.objects.all().order_by('-id') # -는 역순을 의미 # 모든 게시물을 가지고 올껀데 역순이여서 최신순으로 가져올 것이다. 
+    page = int(request.GET.get('p', 1))
+    paginator = Paginator(all_boards, 2) #한페이지에 2개씩 나오게
+
+    boards = paginator.get_page(page)
     return render(request, 'board_list.html', {'boards': boards})
